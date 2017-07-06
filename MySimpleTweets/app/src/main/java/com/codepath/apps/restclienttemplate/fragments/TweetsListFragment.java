efd94b6390b3a1412bd4374267adefd59d0be22b
Ruben Dicker <rdicker@fb.com>
2017-07-06 15:37:47 -0700

    Fixed refresh layout

--- MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/fragments/TweetsListFragment.java
@@ -1,22 +1,31 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.TweetAdapter;
+import com.codepath.apps.restclienttemplate.TwitterApp;
+import com.codepath.apps.restclienttemplate.TwitterClient;
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by rdicker on 7/3/17.
@@ -27,6 +36,8 @@ public class TweetsListFragment extends Fragment {
     TweetAdapter tweetAdapter;
     ArrayList<Tweet> tweets;
     RecyclerView rvTweets;
+    SwipeRefreshLayout swipeContainer;
+    TwitterClient client;
 
     // inflation happens inside onCreateView
 
@@ -50,6 +61,31 @@ public class TweetsListFragment extends Fragment {
         rvTweets.setLayoutManager(new LinearLayoutManager(getContext()));
         // set the adapter
         rvTweets.setAdapter(tweetAdapter);
+        client = TwitterApp.getRestClient();
+
+
+
+
+
+        // Lookup the swipe container view
+        swipeContainer = (SwipeRefreshLayout) v.findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchTimelineAsync(0);
+            }
+        });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
 
 
         return v;
@@ -60,6 +96,12 @@ public class TweetsListFragment extends Fragment {
         tweetAdapter.notifyItemInserted(0);
         rvTweets.scrollToPosition(0);
     }
+
+
+
+
+
+
 
 
 
@@ -78,5 +120,50 @@ public class TweetsListFragment extends Fragment {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+    }
+
+
+    public void fetchTimelineAsync(int page) {
+        // Send the network request to fetch the updated data
+        // `client` here is an instance of Android Async HTTP
+        // getHomeTimeline is an example endpoint.
+
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response)
+            {
+                // Remember to CLEAR OUT old items before appending in the new ones
+                tweetAdapter.clear();
+                // ...the data has come back, add new items to your adapter...
+                for (int i = 0; i < response.length(); i++) {
+                    //convert each object to tweet model
+                    //add that Tweet model to data source
+                    //notify adapter that we've added an item
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(i - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+            public void onFailure(int statusCode, PreferenceActivity.Header[] headers, String responseString, Throwable throwable) {
+                Log.d("TwitterClient", responseString);
+                throwable.printStackTrace();
+            }
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+        });
     }
 }
--- MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/fragments/UserTimelineFragment.java
+++ MySimpleTweets/app/src/main/java/com/codepath/apps/restclienttemplate/fragments/UserTimelineFragment.java
@@ -1,14 +1,17 @@
 package com.codepath.apps.restclienttemplate.fragments;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.codepath.apps.restclienttemplate.TwitterApp;
 import com.codepath.apps.restclienttemplate.TwitterClient;
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
@@ -28,6 +31,9 @@ public class UserTimelineFragment extends TweetsListFragment {
         Bundle args =  new Bundle();
         args.putString("screen_name", screenName);
         userTimelineFragment.setArguments(args);
+
+        // userTimelineFragment.fetchTimelineAsync(0);
+
         return userTimelineFragment;
     }
 
@@ -40,7 +46,6 @@ public class UserTimelineFragment extends TweetsListFragment {
 
         populateTimeline();
     }
-
 
 
     private void populateTimeline() {
@@ -79,6 +84,49 @@ public class UserTimelineFragment extends TweetsListFragment {
         });
     }
 
+    @Override
+    public void fetchTimelineAsync(int page) {
+        // Send the network request to fetch the updated data
+        // `client` here is an instance of Android Async HTTP
+        // getHomeTimeline is an example endpoint.
 
+        String screenName = getArguments().getString("screen_name");
+        client.getUserTimeline(screenName, new JsonHttpResponseHandler() {
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response)
+            {
+                // Remember to CLEAR OUT old items before appending in the new ones
+                tweetAdapter.clear();
+                // ...the data has come back, add new items to your adapter...
+                for (int i = 0; i < response.length(); i++) {
+                    //convert each object to tweet model
+                    //add that Tweet model to data source
+                    //notify adapter that we've added an item
+                    try {
+                        Tweet tweet = Tweet.fromJSON(response.getJSONObject(i));
+                        tweets.add(tweet);
+                        tweetAdapter.notifyItemInserted(i - 1);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // Now we call setRefreshing(false) to signal refresh has finished
+                swipeContainer.setRefreshing(false);
+            }
+            public void onFailure(int statusCode, PreferenceActivity.Header[] headers, String responseString, Throwable throwable) {
+                Log.d("TwitterClient", responseString);
+                throwable.printStackTrace();
+            }
 
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
+                Log.d("TwitterClient", errorResponse.toString());
+                throwable.printStackTrace();
+            }
+
+        });
+    }
 }
